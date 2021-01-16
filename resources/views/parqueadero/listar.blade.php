1e9549599d8458c2fc54759319bb39413db67c8f
Juan C. Cruz <juancruz@udenar.edu.co>
2021-01-16 15:11:20 -0500

    Primer Sprint parqueadero_PCuellar_JCruz

---
+++ resources/views/parqueadero/listar.blade.php
@@ -0,0 +1,32 @@
+@extends('layout.master')
+    @section('content')
+
+    <h2><font color="black"><center>Listado de Clientes </center></font></h2>
+
+    <div class="card-tittle">
+        </br><h2><center>Listado de Clientes</center></h2>      
+    </div>
+
+    <br>
+    <div class ="container">
+        <div class ="row">
+            @foreach($clientes as $p )
+                <div class="card mb-3" style="max-width: 540px;">
+                   
+                        <div class="col-md-25">
+                            <div class="card-body">
+                                <h6 class="card-tittle">{{$p->nombre}}</h6>
+                                <p class="card-text" aling="justify"> Celuda: {{$p->cedula}}</p>
+                                <p class="card-text" aling="justify"> Celular: {{$p->celular}}</p>
+                                <p class="card-text" aling="justify"> Domicilio: {{$p->direccion}}</p>
+                                <p class="card-text" aling="justify"> correo: {{$p->correo}}</p>
+                                <br>
+                            </div>
+                        </div>
+                  
+                </div>
+            @endforeach 
+        </div>
+    </div> 
+
+@stop
